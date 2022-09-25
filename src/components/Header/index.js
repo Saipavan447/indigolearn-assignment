38a0894d43207cb9ead72dcd06147fbd2ba54eb9
Saipavan447 <pavankrishna529@gmail.com>
2022-09-25 13:50:45 +0000

    commit message

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -19,26 +19,27 @@ import ModeEditIcon from '@mui/icons-material/ModeEdit'
 import DescriptionIcon from '@mui/icons-material/Description'
 import LiveTvIcon from '@mui/icons-material/LiveTv'
 import StayPrimaryPortraitIcon from '@mui/icons-material/StayPrimaryPortrait'
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
 import {Component} from 'react'
 import './index.css'
 
 class Header extends Component {
-  state = {showRoutingSection: false}
+  state = {navBarSection: false}
 
-  showBottomBar = () => {
+  leftSideBar = () => {
     this.setState(prevState => ({
-      showRoutingSection: !prevState.showRoutingSection,
+      navBarSection: !prevState.navBarSection,
     }))
   }
 
   render() {
-    const {showRoutingSection} = this.state
+    const {navBarSection} = this.state
     return (
       <div className="nav-container">
         <nav className="navbar-container">
           <div className="nav-start">
             <button className="nav-button" type="button">
-              <GoThreeBars size={25} onClick={this.showBottomBar} />
+              <GoThreeBars size={25} onClick={this.leftSideBar} />
             </button>
             <h1 className="nav-heading">IndigoLearn</h1>
           </div>
@@ -55,80 +56,82 @@ class Header extends Component {
             <ShoppingCartIcon />
           </button>
         </nav>
-        <nav className={showRoutingSection ? 'nav-menu active' : 'nav-menu'}>
-          <ul className="nav-menu-items" onClick={this.showBottomBar}>
-            <li className="navbar-toggle">
-              <h1>Indigolearn</h1>
-              <ArrowBackIcon className="menu-bars" />
+        <nav className={navBarSection ? 'nav-bar active' : 'nav-bar'}>
+          <ul className="nav-bar-items" onClick={this.leftSideBar}>
+            <li className="navbar-logo">
+              <h1 className="nav-heading">Indigolearn</h1>
+              <ArrowBackIcon className="close-icon" />
             </li>
-            <AccountCircleIcon size={30} className="icon3" />
-            <p>Login/Signup</p>
-
-            <li className="slider-li">
-              <ImFilesEmpty className="slider-icons" />
+            <AccountCircleOutlinedIcon
+              fontSize="large"
+              className="profile-nav-icon"
+            />
+            <p className="login-nav">Login/Signup</p>
+            <hr className="line-break" />
+            <li className="nav-description">
+              <ImFilesEmpty className="nav-icons" />
               <span>Buy Courses</span>
               <ChevronRightIcon />
             </li>
-            <li className="slider-li">
-              <ForwardIcon className="slider-icons" />
+            <li className="nav-description">
+              <ForwardIcon className="nav-icons" />
               <span>Programs</span>
-
               <ChevronRightIcon />
             </li>
-            <li className="slider-li">
-              <SiGooglescholar className="slider-icons" />
+            <li className="nav-description">
+              <SiGooglescholar className="nav-icons" />
               <span>Scholarship</span>
             </li>
-            <li className="slider-li">
-              <SubscriptionsIcon className="slider-icons" />
+            <li className="nav-description">
+              <SubscriptionsIcon className="nav-icons" />
               <span>Free Courses</span>
             </li>
-            <li className="slider-li">
-              <FolderSpecialIcon className="slider-icons" />
+            <li className="nav-description">
+              <FolderSpecialIcon className="nav-icons" />
               <span>Free Resources</span>
             </li>
-            <li className="slider-li">
-              <DateRangeIcon className="slider-icons" />
+            <li className="nav-description">
+              <DateRangeIcon className="nav-icons" />
               <span>Study Planner</span>
             </li>
-            <li className="slider-li">
-              <AssignmentIcon className="slider-icons" />
+            <li className="nav-description">
+              <AssignmentIcon className="nav-icons" />
               <span>MCQs</span>
             </li>
-            <li className="slider-li">
-              <ForumOutlinedIcon className="slider-icons" />
+            <li className="nav-description">
+              <ForumOutlinedIcon className="nav-icons" />
               <span>Formus</span>
             </li>
-            <li className="slider-li">
-              <BusinessOutlinedIcon className="slider-icons" />
+            <li className="nav-description">
+              <BusinessOutlinedIcon className="nav-icons" />
               <span>Firm Reviews</span>
             </li>
-            <li className="slider-li">
-              <PlayCircleOutlineOutlinedIcon className="slider-icons" />
+            <li className="nav-description">
+              <PlayCircleOutlineOutlinedIcon className="nav-icons" />
               <span>Free Videos</span>
             </li>
-            <li className="slider-li">
-              <NewspaperOutlinedIcon className="slider-icons" />
+            <li className="nav-description">
+              <NewspaperOutlinedIcon className="nav-icons" />
               <span>News Updates</span>
             </li>
-            <li className="slider-li">
-              <ModeEditIcon className="slider-icons" />
+            <li className="nav-description">
+              <ModeEditIcon className="nav-icons" />
               <span>Course Guides</span>
             </li>
-            <li className="slider-li">
-              <LiveHelpOutlinedIcon className="slider-icons" />
+            <li className="nav-description">
+              <LiveHelpOutlinedIcon className="nav-icons" />
               <span>FAQ</span>
             </li>
-            <li className="slider-li">
-              <DescriptionIcon className="slider-icons" />
+            <li className="nav-description">
+              <DescriptionIcon className="nav-icons" />
               <span>Blogs</span>
             </li>
-            <li className="slider-li">
-              <LiveTvIcon className="slider-icons" />
+            <li className="nav-description">
+              <LiveTvIcon className="nav-icons" />
               <span>Youtube Live</span>
             </li>
-            <li className="slider-li">
-              <StayPrimaryPortraitIcon className="slider-icons" />
+            <li className="nav-description">
+              <StayPrimaryPortraitIcon className="nav-icons" />
               <span>APP 1FIN</span>
             </li>
           </ul>
